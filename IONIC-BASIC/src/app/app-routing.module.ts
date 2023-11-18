132295f0875eebf1aec697df4c9c8fc5baf467f5
ambararr <141696462+ambararr@users.noreply.github.com>
2023-11-18 00:03:49 -0600

    pract 13

--- IONIC-BASIC/src/app/app-routing.module.ts
+++ IONIC-BASIC/src/app/app-routing.module.ts
@@ -75,6 +75,10 @@ const routes: Routes = [
       {
         path: 'destinos-api',
         loadChildren: () => import('./destinos-api/destinos-api.module').then( m => m.DestinosApiPageModule)
+      },
+      {
+        path: 'galeria',
+        loadChildren: () => import('./galeria/galeria.module').then( m => m.GaleriaPageModule)
       }
     ],
     canActivate: [AutGuardGuard]
@@ -86,6 +90,10 @@ const routes: Routes = [
   {
     path: 'register',
     loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'galeria',
+    loadChildren: () => import('./galeria/galeria/galeria.module').then( m => m.GaleriaPageModule)
   },
   
 ];
